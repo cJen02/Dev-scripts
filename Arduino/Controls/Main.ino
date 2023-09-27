64661da32c048c88a1aa35e29a30292dc0cc9469
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2023-09-27 11:00:35 +0500

    Create Main.ino

---
+++ Arduino/Controls/Main.ino
@@ -0,0 +1,19 @@
+/*
+
+# Torque calc (A_In) -> T
+- subroutine core
+
+# Position calc (Encoder - dir, pulse) -> Q
+- 
+
+# PI control (Q/T) -> Null
+- 
+
+# Torque control (T) -> Null
+- subroutine core
+
+*/
+
+
+void setup() {}
+void loop() {}
