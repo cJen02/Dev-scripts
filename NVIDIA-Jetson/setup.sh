77a5e6711acdb21a52baeecdb71538343c902364
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2024-10-28 10:38:57 +0530

    Update setup.sh

--- NVIDIA-Jetson/setup.sh
+++ NVIDIA-Jetson/setup.sh
@@ -1,5 +1,5 @@
 
-# Python issues
+## Python issues
 sudo apt purge -y python2.7-minimal
 sudo apt purge -y python3.6-minimal
 sudo apt install python3.8
@@ -8,9 +8,15 @@ sudo apt install -y python3-pip
 sudo ln -s /usr/bin/pip3 /usr/bin/pip
 python --version
 
+## GPIO packages
+sudo pip3 install Jetson.GPIO
+sudo groupadd -f -r gpio
+sudo usermod -a -G gpio {username}
+
 
 ## Install VScode
-git clone https://github.com/JetsonHacksNano/installVSCode.git
-cd installVSCode
-./installVSCode.sh
+VERSION=1.94.2
+wget -N -O vscode-linux-deb.arm64.deb https://update.code.visualstudio.com/$VERSION/linux-deb-arm64/stable
+sudo apt install ./vscode-linux-deb.arm64.deb
+
 
