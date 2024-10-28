12aa44ce86b28e3a84c1cec7cc6aa4afdb521b4e
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2024-10-28 10:21:08 +0530

    Update setup.sh

--- NVIDIA-Jetson/setup.sh
+++ NVIDIA-Jetson/setup.sh
@@ -1,4 +1,14 @@
 
+# Python issues
+sudo apt purge -y python2.7-minimal
+sudo apt purge -y python3.6-minimal
+sudo apt install python3.8
+sudo ln -s /usr/bin/python3.8 /usr/bin/python
+sudo apt install -y python3-pip
+sudo ln -s /usr/bin/pip3 /usr/bin/pip
+python --version
+
+
 ## Install VScode
 git clone https://github.com/JetsonHacksNano/installVSCode.git
 cd installVSCode
