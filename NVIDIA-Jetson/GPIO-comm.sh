6be719c8541a12e6e165358293f80c5720f133d4
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2024-11-08 11:53:58 +0530

    Update GPIO-comm.sh

--- NVIDIA-Jetson/GPIO-comm.sh
+++ NVIDIA-Jetson/GPIO-comm.sh
@@ -31,3 +31,25 @@ sudo apt-get install -y python-setuptools
 sudo python setup.py install
 sudo python3 setup.py install
 sudo modprobe spidev # Load the spidev driver by below command first
+
+"""
+#!/usr/bin/python3
+
+import spidev
+import time
+import binascii
+
+spi = spidev.SpiDev() # Tạo đối tượng cho SPI
+spi.open(0, 0) # mở port 0, device (CS) 0
+#spi.open(1, 0) # mở port 0, device (CS) 0
+spi.max_speed_hz = 500000
+#spi.max_speed_hz = 11000000
+
+try:
+  while True:
+    print("abc")
+    resp = spi.xfer2([0x61,0x62,0x63,0xA]) # [‘a’,‘b’,‘c’,‘\n’]
+    time.sleep(1) # sleep for 1 seconds
+except KeyboardInterrupt:
+  spi.close()
+"""
