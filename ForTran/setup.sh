620ff2d15651588d11d5149660d2fa5af32c6e3a
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2025-01-02 11:52:02 +0530

    Update setup.sh

--- ForTran/setup.sh
+++ ForTran/setup.sh
@@ -1,3 +1,14 @@
 # Ref:- https://laptops.eng.uci.edu/engineering-software/programming-basics/gfortran-hello-world
 sudo apt-get install gfortran  # Linux
 which gfortran
+which pico
+
+
+## "Hello World" Program
+pico helloworld.f90
+"""
+program helloworld
+      print *, "Hello World"
+end program helloworld
+"""
+gfortran helloworld.f90 -o hello
