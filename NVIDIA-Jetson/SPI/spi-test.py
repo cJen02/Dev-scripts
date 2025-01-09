a0094b9b6a1b9ee7b10b5af8f970311bb6a94682
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2025-01-10 01:07:14 +0530

    Create spi-test.py

---
+++ NVIDIA-Jetson/SPI/spi-test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import spidev
+import time
+import binascii
+
+spi = spidev.SpiDev() # Tạo đối tượng cho SPI
+spi.open(0, 0) # mở port 0, device (CS) 0
+#spi.open(1, 0) # mở port 0, device (CS) 0
+spi.max_speed_hz = 500000
+#spi.max_speed_hz = 11000000
+
+try:
+  while True:
+    print(“abc”)
+    resp = spi.xfer2([0x61,0x62,0x63,0xA]) # [‘a’,‘b’,‘c’,‘\n’]
+    time.sleep(1) # sleep for 1 seconds
+except KeyboardInterrupt:
+  spi.close()
