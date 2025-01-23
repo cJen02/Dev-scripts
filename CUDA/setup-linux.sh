683acc2c64e09a59c62ae5af0d78f460add21fa8
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2025-01-23 14:05:48 +0530

    Update setup-linux.sh

--- CUDA/setup-linux.sh
+++ CUDA/setup-linux.sh
@@ -1,3 +1,5 @@
+# Ref- https://developer.nvidia.com/cuda-12-2-0-download-archive?target_os=Linux&target_arch=x86_64&Distribution=Ubuntu&target_version=20.04&target_type=deb_local
+
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget https://developer.download.nvidia.com/compute/cuda/12.2.0/local_installers/cuda-repo-ubuntu2004-12-2-local_12.2.0-535.54.03-1_amd64.deb
