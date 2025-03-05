692a2ceef71970c7eca1d23f0e914c577341d623
Jenish Chauhan <86952828+cJen02@users.noreply.github.com>
2025-03-05 10:04:43 +0530

    Update pySetUp.sh

--- @Cluster/pySetUp.sh
+++ @Cluster/pySetUp.sh
@@ -1,9 +1,11 @@
+module load python/3.10
 python3.10 -m venv ./.venv
 ls -a
 
 source .venv/bin/activate
 
-# Training files
-
+# packages
+pip install myosuite
+pip install stable_baselines3
 
 deactivate
